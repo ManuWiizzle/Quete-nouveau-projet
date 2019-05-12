01f754368bbba560d642246def8b520aa97bb027
Manuel <manu.wiizzle@gmail.com>
2019-05-12 21:08:43 +0200

    Afficher une page de bienvenue

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,18 @@
+<?php
+// src/Controller/BlogController.php
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class BlogController extends AbstractController
+{
+    public function index()
+    {
+        return $this->render('blog/index.html.twig', [
+            'owner' => 'Manuel',
+        ]);
+    }
+}
